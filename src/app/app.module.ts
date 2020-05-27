be887fcc915010f769dd3be1abbea95623693962
LayCraft <curtis.laycraft@zoho.com>
2020-05-27 14:10:17 -0700

    messing about with styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
@@ -51,6 +53,8 @@ import { ToneControlsComponent } from './tone-controls/tone-controls.component';
     MatSliderModule,
     MatSnackBarModule,
     MatToolbarModule,
+    MatInputModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
