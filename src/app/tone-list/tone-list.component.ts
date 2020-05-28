47c66c76ecada7a5ec4a140d0f7c21210398d972
LayCraft <curtis.laycraft@zoho.com>
2020-05-27 17:08:40 -0700

    produce tone

--- src/app/tone-list/tone-list.component.ts
+++ src/app/tone-list/tone-list.component.ts
@@ -7,20 +7,30 @@ import { ISound } from '../interfaces/sound.interface';
   styleUrls: ['./tone-list.component.scss']
 })
 export class ToneListComponent implements OnInit {
-
   tones: ISound[] = [];
+  oscillators: any[] = []; // collection of sounds.
+  context; // audio context
 
   constructor() { }
 
   ngOnInit(): void {
+    // set up audio context
+    this.setupAudio();
+
     // be sure there is at least one tone
     if (this.tones.length === 0) {
       this.addTone();
     }
   }
 
+  setupAudio() {
+    // make context for this page
+    this.context = new AudioContext();
+  }
+
   play(index: number, playing: boolean) {
     this.tones[index].playing = playing;
+    playing ? this.oscillators[index].start() : this.oscillators[index].stop();
   }
 
   deleteTone(index: number) {
@@ -29,14 +39,26 @@ export class ToneListComponent implements OnInit {
   }
 
   addTone() {
-    this.tones.push({
+    const defaultTone = {
       name: '',
       description: '',
       frequency: 8000,
       playing: false,
       volume: 100,
       waveType: 'sine'
-    });
+    };
+
+    // add a tone
+    this.tones.push(defaultTone);
+
+    // add a matching oscillator
+    const oscillator = this.context.createOscillator();
+    // set oscillator type
+    oscillator.type = defaultTone.waveType;
+    // oscillator.frequency = defaultTone.frequency;
+    oscillator.connect(this.context.destination);
+    // add it to ready list
+    this.oscillators.push(oscillator);
   }
   cloneTone(index: number) {
     // make a shallow copy of the properties
